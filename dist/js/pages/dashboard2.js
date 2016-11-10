0da428e2476c12758e357af3e5d2c65591c6a800
AmyCeleste <amycelestestewart@gmail.com>
2016-11-10 11:44:14 -0600

    added status overview chart

--- dist/js/pages/dashboard2.js
+++ dist/js/pages/dashboard2.js
@@ -133,5 +133,60 @@ $(function () {
   //- END PIE CHART -
   //-----------------
 
+  var pieChartCanvas = $("#pieChart2").get(0).getContext("2d");
+  var pieChart2 = new Chart(pieChartCanvas);
+  var PieData2 = [
+    {
+      value: 100,
+      color: "#00a65a",
+      highlight: "#00a65a",
+      label: "Good"
+    },
+    {
+      value: 67,
+      color: "#f39c12",
+      highlight: "#f39c12",
+      label: "Doubtful"
+    },
+    {
+      value: 48,
+      color: "#f56954",
+      highlight: "#f56954",
+      label: "Fail"
+    }
+  ];
+  var pieOptions = {
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke: true,
+    //String - The colour of each segment stroke
+    segmentStrokeColor: "#fff",
+    //Number - The width of each segment stroke
+    segmentStrokeWidth: 1,
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout: 50, // This is 0 for Pie charts
+    //Number - Amount of animation steps
+    animationSteps: 100,
+    //String - Animation easing effect
+    animationEasing: "easeOutBounce",
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate: true,
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale: false,
+    //Boolean - whether to make the chart responsive to window resizing
+    responsive: true,
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio: false,
+    //String - A legend template
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+    //String - A tooltip template
+    tooltipTemplate: "<%=value %> <%=label%>"
+  };
+  //Create pie or douhnut chart
+  // You can switch between pie and douhnut using the method below.
+  pieChart2.Doughnut(PieData2, pieOptions);
+  //-----------------
+  //- END PIE CHART -
+  //-----------------
+
 
 });
